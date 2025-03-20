6245ed52f2ad10c4eee896a057230ec0e9ea9cb7
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-20 08:12:38 +0700

    Create a new board recorded in database

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,3 +1,4 @@
+import { boardModel } from "../models/boardModel";
 import { slugify } from "../utils/formatters";
 
 // xử lý logic : controller là xử lý điều hướng xong chuyển sang service
@@ -10,12 +11,19 @@ const createNew = async (reqbody) => {
         };
 
         // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+        const createdBoard = await boardModel.createNew(newBoard);
+
+        //Lấy bản ghi board sau khi gọi ( tùy mục đích dự án mà có cần bước này ko)
+        const getNewBoard = await boardModel.findOneById(
+            createdBoard.insertedId
+        );
+        console.log(getNewBoard);
 
         // Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án
         // Bắn email, notification về cho admin khi có 1 cái board được tạo,...
 
         // Trả kết quả về, trong Service luôn phải có return
-        return newBoard;
+        return getNewBoard;
     } catch (error) {
         throw error;
     }
