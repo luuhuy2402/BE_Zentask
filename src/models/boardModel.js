08ba898cd8fef8611228fbe85ae845bc2a54adad
luuhuy2402 <xeoxeo1895@gmail.com>
2025-03-20 09:19:43 +0700

    Check data in both validation and model schema

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "../utils/validators";
 import { GET_DB } from "../config/mongodb";
+import { ObjectId } from "mongodb";
 
 //Define Collection (Name & Schema)
 const BOARD_COLLECTION_NAME = "boards";
@@ -20,11 +21,18 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
     _destroy: Joi.boolean().default(false),
 });
 
+const validateBeforeCreate = async (data) => {
+    return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+        abortEarly: false,
+    });
+};
+
 const createNew = async (data) => {
     try {
+        const validData = await validateBeforeCreate(data);
         const createdBoard = await GET_DB()
             .collection(BOARD_COLLECTION_NAME)
-            .insertOne(data);
+            .insertOne(validData);
         return createdBoard;
     } catch (error) {
         throw new Error(error);
@@ -36,7 +44,7 @@ const findOneById = async (id) => {
         const result = await GET_DB()
             .collection(BOARD_COLLECTION_NAME)
             .findOne({
-                _id: id,
+                _id: new ObjectId(id),
             });
         return result;
     } catch (error) {
