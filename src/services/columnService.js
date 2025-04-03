4d36b31e7745a3dc3492fb2aa3ad738dd86dacb3
luuhuy2402 <xeoxeo1895@gmail.com>
2025-04-03 09:03:06 +0700

    Delete Column id in the array ColumnOrderIds of the Board

--- src/services/columnService.js
+++ src/services/columnService.js
@@ -1,7 +1,8 @@
-import { get } from "lodash";
 import { boardModel } from "../models/boardModel";
 import { columnModel } from "../models/columnModel";
 import { cardModel } from "../models/cardModel";
+import { StatusCodes } from "http-status-codes";
+import ApiError from "../utils/ApiError";
 
 const createNew = async (reqbody) => {
     try {
@@ -42,10 +43,17 @@ const update = async (columnId, reqBody) => {
 
 const deleteItem = async (columnId) => {
     try {
+        const targetColumn = await columnModel.findOneById(columnId);
+        if (!targetColumn) {
+            throw new ApiError(StatusCodes.NOT_FOUND, "Column not found!");
+        }
         //Xóa column
         await columnModel.deleteOneById(columnId);
         //xóa toàn bộ card thuộc column xóa
         await cardModel.deleteManyByColumnId(columnId);
+
+        //Xóa columnId trong mảng columnOrderIds của Board chứa column xóa
+        await boardModel.pullColumnOrderIds(targetColumn);
 
         return { deleteResult: "Column and its Cards deleted successfully" };
     } catch (error) {
